633f3f94cee935ea6dcc52c7e196f89d2d0e91fa
Zuraw7 <zurawicki123@gmail.com>
2026-05-27 02:28:03 +0200

    Added documentation for ThreadSafeQueue class

--- includes/thread/thread_safe_queue.hpp
+++ includes/thread/thread_safe_queue.hpp
@@ -5,19 +5,36 @@
 #include <mutex>
 #include <stdexcept>
 
+/**
+ * @brief Templated thread-safe queue backed by std::deque.
+ *
+ * Multiple threads can push and pop concurrently without data races.
+ * All operations are protected by a single mutex.
+ *
+ * @tparam TType Type of elements stored in the queue.
+ *
+ * @var m_queue Internal deque holding the elements.
+ * @var m_mutex Protects all accesses to m_queue.
+ */
 template<typename TType>
 class ThreadSafeQueue {
 public:
+    /** @brief Adds an element at the end of the queue. */
     void push_back(const TType &newElement){
         std::lock_guard<std::mutex> lock(m_mutex);
         m_queue.push_back(newElement);
     }
 
+    /** @brief Adds an element at the front of the queue. */
     void push_front(const TType &newElement) {
         std::lock_guard<std::mutex> lock(m_mutex);
         m_queue.push_front(newElement);
     }
 
+    /**
+     * @brief Extracts and returns the last element.
+     * @throws std::runtime_error if the queue is empty.
+     */
     TType pop_back() {
         std::lock_guard<std::mutex> lock(m_mutex);
         if (m_queue.empty())
@@ -28,6 +45,10 @@ public:
         return element;
     }
 
+    /**
+     * @brief Extracts and returns the first element.
+     * @throws std::runtime_error if the queue is empty.
+     */
     TType pop_front() {
         std::lock_guard<std::mutex> lock(m_mutex);
         if (m_queue.empty())
