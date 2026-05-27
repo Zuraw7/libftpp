fd87b861ef01ed71712295ea35ac25625a85a36c
Zuraw7 <zurawicki123@gmail.com>
2026-05-27 02:58:42 +0200

    Added Thread class

---
+++ includes/thread/thread.hpp
@@ -0,0 +1,54 @@
+#ifndef THREAD_HPP
+#define THREAD_HPP
+
+#include "../iostream/thread_safe_iostream.hpp"
+#include <functional>
+#include <thread>
+
+extern thread_local ThreadSafeIOStream threadSafeCount;
+
+/**
+ * @brief Wrapper around std::thread with a name used as a ThreadSafeIOStream prefix.
+ *
+ * The thread does not start automatically - call start() explicitly after construction.
+ * Use stop() to join the thread before the object is destroyed, otherwise std::terminate
+ * will be called.
+ *
+ * Any function passed to the constructor that uses threadSafeCount will automatically
+ * have its output prefixed with "[name] ".
+ *
+ * @var m_name   Name of the thread, used as prefix for threadSafeCount output.
+ * @var m_func   Function executed when start() is called.
+ * @var m_thread Underlying std::thread, created on start().
+ *
+ * @example
+ * @code
+ * Thread t("worker", []() {
+ *     threadSafeCount << "hello\n";  // prints: [worker] hello
+ * });
+ * t.start();
+ * t.stop();
+ * @endcode
+ */
+class Thread {
+public:
+    /**
+     * @brief Sets up thread data. Does not start execution.
+     * @param name          Name used as threadSafeCount prefix inside the thread.
+     * @param funcToExecute Function to run when start() is called.
+     */
+    Thread(const std::string& name, std::function<void()> funcToExecute);
+
+    /** @brief Launches the thread, executing the function passed to the constructor. */
+    void start();
+
+    /** @brief Joins the thread, blocking until it finishes execution. */
+    void stop();
+
+private:
+    std::string m_name;
+    std::function<void()> m_func;
+    std::thread m_thread;
+};
+
+#endif
